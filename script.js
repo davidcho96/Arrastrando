d4930256c450e23e2c40edeb4e1895b4181555d3
David Maldonado <david_mc8@hotmail.com>
2018-02-14 23:11:20 -0300

    Intento promedio

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let arr_notas = [...document.querySelectorAll('.input-nota')];
 
 let arr_porcentaje = [...document.querySelectorAll('.input-porcentaje')];
 
+let promedio = 0;
+
 btn.addEventListener ('click', function(){
 
 	let html = '<li> <input type="text" id="input-nota'+(arr_notas.length + 1)+'" class="input-nota" value="0" placeholder="Ingresa una nota"> <input type="text" id="input-porcentaje'+(arr_porcentaje.length + 1)+'" class="input-porcentaje" value="0" placeholder="Ingresa un porcentaje"> </li>';
@@ -17,6 +19,25 @@ btn.addEventListener ('click', function(){
 	
 	arr_porcentaje = [...document.querySelectorAll('.input-porcentaje')];
 
-	console.log(arr_notas);
+	console.log(arr_notas.textContent);
+
 	console.log(arr_porcentaje);
 });
+
+arr_notas.forEach(function(elem, index){
+	elem.addEventListener ('blur', function(){
+		console.log('hola');
+		obt_promedio();
+	});
+});
+
+function obt_promedio() {
+	for(let i=0;i<=arr_notas.length;i++){
+		for (let j=0;j<=arr_porcentaje.length;j++){
+			if (i == j){
+				promedio = parseInt(arr_notas[i])*parseInt(arr_porcentaje[j]);
+				console.log(promedio);
+			}
+		}
+	}
+}

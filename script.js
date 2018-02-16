5c4cf774e07e400ce99f0f8fd136a35e7c0e6b27
David Maldonado <david_mc8@hotmail.com>
2018-02-16 01:02:08 -0300

    Modificación Script 90% hecho de la funcionalidad

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
-
-let ul = document.getElementById('list-inputs');
-
-let btn = document.getElementById('add-li');
-
-let arr_notas = [...document.querySelectorAll('.input-nota')];
-
+/*------------------------Variables-------------------------------------------------------------------------------------*/
+let inputs_list = document.getElementById('list-inputs');
+let btn_add = document.getElementById('add-li');
+var arr_notas = [...document.querySelectorAll('.input-nota')];
 let arr_porcentaje = [...document.querySelectorAll('.input-porcentaje')];
-
 var promedio = 0;
-
-btn.addEventListener ('click', function(){
-
-	let html = '<li> <input type="text" id="input-nota'+(arr_notas.length + 1)+'" class="input-nota" value="0" placeholder="Ingresa una nota"> <input type="text" id="input-porcentaje'+(arr_porcentaje.length + 1)+'" class="input-porcentaje" value="0" placeholder="Ingresa un porcentaje"> </li>';
-
-	ul.insertAdjacentHTML('beforeend', html);
-
-	arr_notas = [...document.querySelectorAll('.input-nota')];
+/*------------------------Funciones-------------------------------------------------------------------------------------*/
+btn_add.addEventListener ('click', function(){
+	let html_li = '<li> <input type="text" id="input-nota'+(arr_notas.length + 1)+'" class="input-nota" value="0" placeholder="Ingresa una nota"> <input type="text" id="input-porcentaje'+(arr_porcentaje.length + 1)+'" class="input-porcentaje" value="0" placeholder="Ingresa un porcentaje"> </li>';
 	
+	inputs_list.insertAdjacentHTML('beforeend', html_li);
+	
+	arr_notas = [...document.querySelectorAll('.input-nota')];
 	arr_porcentaje = [...document.querySelectorAll('.input-porcentaje')];
 
-	console.log(arr_notas.textContent);
-
-	console.log(arr_porcentaje);
+	calcular();
 });
-arr_notas.forEach(function(elem, index){
-	elem.addEventListener ('blur', function(){
-		promedio = 0;
-		var a = 0;
-				for (var i = 0; i<=arr_porcentaje.length - 1 ; i++) {
-					a += parseInt(arr_porcentaje[i].value);
-				}
-		arr_notas.forEach(function(w, ind){
-
-				promedio += parseInt(arr_notas[ind].value) * parseInt(arr_porcentaje[ind].value) / a;
-				// console.log(promedio);
-
-
-
-			// c += parseInt(w.value);
-			// obt_promedio(i);
-		});
-		console.log(promedio);
-		/*promedio += parseInt(arr_notas[index].value) + parseInt(arr_porcentaje[index].value);
-		console.log(promedio);*/
-		// console.log(parseInt(arr_notas[index].value) + parseInt(arr_porcentaje[index].value));
+/*----------------------------------------------------------------------------------------------------------------------*/
+calcular();
+/*----------------------------------------------------------------------------------------------------------------------*/
+function calcular(){
+	arr_notas.forEach(function(element1, index){
+		events(element1);
 	});
-});
-
-function obt_promedio(ind) {
-	/*var promedio = 0;
-	var a = 0;
-		for (var i = 0; i<=arr_porcentaje.length - 1 ; i++) {
-			a += parseInt(arr_porcentaje[i].value);
-		}*/
-		// console.log(a);
-
-	promedio = parseInt(arr_notas[ind].value) * parseInt(arr_porcentaje[ind].value) / a;
-	console.log(promedio);
-	/*for(let i=0;i<=arr_notas.length;i++){
-		for (let j=0;j<=arr_porcentaje.length;j++){
-			if (i == j){
-				promedio = parseInt(arr_notas[i])*parseInt(arr_porcentaje[j]);
-				console.log(promedio);
-			}
+	arr_porcentaje.forEach(function(element2, index2){
+		events(element2);
+	});
+}
+/*----------------------------------------------------------------------------------------------------------------------*/
+function events(elementos){
+	elementos.addEventListener ('blur', function(){
+		if(elementos.value == '' || isNaN(elementos.value) ){
+			elementos.value = 0;
+		}else{
+			 	obtenerPromedio();
 		}
-	}*/
+	});
+
+	elementos.addEventListener ('focus', function(){
+		if(elementos.value == 0){
+			elementos.value = "";
+		}
+	});
+	
+	elementos.addEventListener ('keyup', function(){
+		if(isNaN(elementos.value) || elementos.value == ""){
+			console.log('Ingresa valores correctos');
+		}else{
+			obtenerPromedio();
+		}
+	});
+}
+
+/*----------------------------------------------------------------------------------------------------------------------*/
+function obtenerPromedio(){
+	promedio = 0;
+	var porcentaje_total = 0;
+		for (var i = 0; i<=arr_porcentaje.length - 1 ; i++) {
+			porcentaje_total += parseInt(arr_porcentaje[i].value);
+		}
+			if(porcentaje_total !== 0){
+				arr_notas.forEach(function(w, ind){
+					promedio += parseInt(arr_notas[ind].value) * parseInt(arr_porcentaje[ind].value) / porcentaje_total;
+				});
+					document.getElementById('pro').textContent = promedio.toFixed(1);
+			}
 }

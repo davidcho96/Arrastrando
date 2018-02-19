c77a23a843b2364d2e394a0881aa90623b0c9ad4
David Maldonado <david_mc8@hotmail.com>
2018-02-18 21:03:28 -0300

    Se añadió 'Dar examen' Se añadió la función dar examen, se incluyen inputs correspondientes y diesño asociado

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let btn_add = document.getElementById('add-li');
 var arr_notas = [...document.querySelectorAll('.input-nota')];
 let arr_porcentaje = [...document.querySelectorAll('.input-porcentaje')];
 var promedio = 0;
+
+var nota_examen = document.getElementById('input-nota-examen');
+
+var porcentaje_examen = document.getElementById('input-porcentaje-examen');
+
+let arr_examen = [nota_examen, porcentaje_examen];
+
+let btn_dar_examen = document.getElementById('btn-dar-examen');
+
+var promedio_examen = 0;
+
 /*------------------------Funciones-------------------------------------------------------------------------------------*/
 btn_add.addEventListener ('click', function(){
 	console.log(arr_notas);
@@ -25,6 +36,9 @@ function calcular(){
 	arr_porcentaje.forEach(function(element2, index2){
 		events(element2);
 	});
+	arr_examen.forEach(function(element3, index3){
+		events(element3);
+	});
 }
 /*----------------------------------------------------------------------------------------------------------------------*/
 function events(elementos){
@@ -32,7 +46,12 @@ function events(elementos){
 		if(elementos.value == '' || isNaN(elementos.value) ){
 			elementos.value = 0;
 		}else{
-			 	obtenerPromedio();
+			if(elementos.id.match(/input-nota-examen*/) || elementos.id.match(/input-porcentaje-examen*/)){
+				obtenerPromedioExamen();
+			}else{
+				obtenerPromedio();
+			 	console.log('adios');
+			}
 		}
 	});
 
@@ -46,7 +65,12 @@ function events(elementos){
 		if(isNaN(elementos.value) || elementos.value == ""){
 			console.log('Ingresa valores correctos');
 		}else{
-			obtenerPromedio();
+			if(elementos.id.match(/input-nota-examen/) || elementos.id.match(/input-porcentaje-examen/)){
+				obtenerPromedioExamen();
+			}else{
+				obtenerPromedio();
+			 	console.log('adios');
+			}
 		}
 	});
 }
@@ -54,7 +78,7 @@ function events(elementos){
 /*----------------------------------------------------------------------------------------------------------------------*/
 function obtenerPromedio(){
 	promedio = 0;
-	var porcentaje_total = 0;
+	let porcentaje_total = 0;
 		for (var i = 0; i<=arr_porcentaje.length - 1 ; i++) {
 			porcentaje_total += parseInt(arr_porcentaje[i].value);
 		}
@@ -64,4 +88,30 @@ function obtenerPromedio(){
 				});
 					document.getElementById('pro').textContent = promedio.toFixed(1);
 			}
+}
+
+btn_dar_examen.addEventListener('click', function(){
+	console.log(arr_examen);
+	if(document.getElementById('contenido-examen').classList.contains('d-none')){
+		document.getElementById('contenido-examen').classList.remove('d-none');
+		document.getElementById('contenido-examen').classList.add('d-flex');
+		btn_dar_examen.textContent = 'Ocultar';
+	}else{
+		document.getElementById('contenido-examen').classList.remove('d-flex');
+		document.getElementById('contenido-examen').classList.add('d-none');
+		btn_dar_examen.textContent = 'Dar Examen';
+	}
+});
+
+function obtenerPromedioExamen(){
+	console.log(promedio);
+	let porcentaje_total = 0;
+		for (var i = 0; i<=arr_porcentaje.length - 1 ; i++) {
+			porcentaje_total += parseInt(arr_porcentaje[i].value);
+		}
+		if(porcentaje_total !== 0){
+			promedio_examen = ((promedio * 75) / (porcentaje_total)) + (parseInt(nota_examen.value) * parseInt(porcentaje_examen.value) / porcentaje_total);
+			document.getElementById('promedio-examen').textContent = promedio_examen.toFixed(1);
+		}
+
 }
